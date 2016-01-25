ec78824cda79b3811ae00b33823658f6dbd89a7a
Student <student@RU-102.local>
2016-01-25 16:58:47 -0700

    gold_baseComplete_bonusI

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 angular.module('goldRush', [])
-angular.module('goldRush').controller('goldMapper', ['$scope', function($scope) {
+
+angular.module('goldRush').
+	.factory('treasurePoints', [function() {
+		var coords= [
+			{
+				coordx: center,
+				coordy: center,
+				note: click to start
+			}
+		
+			}]
+
+		// var notes= ["note0","note1","note2"]
+		// i=((coords.length)-1))
+		
+		// return {
+		// 	coords[i]:note[i]}
+	}]);
+
+
+
+
+angular.module('goldRush')
+	.controller('goldMapper', ['$scope', function($scope) {
 		// $scope.showEvent = function(event) {
 		// 	console.log(event);
 
@@ -9,6 +32,11 @@ angular.module('goldRush').controller('goldMapper', ['$scope', function($scope) 
 		$scope.clickEvent = function(event) {
 			$scope.x = ((event.clientX) - 16);
 			$scope.y = ((event.clientY) - 37);
+			push{
+				x: event.clientX
+				y: $scope.y
+				notes: 
+			}
 			$scope.showIcon = !$scope.showIcon;
 			console.log(event.pageX);
 			console.log(event.pageY);
