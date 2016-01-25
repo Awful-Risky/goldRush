77814574d0047bddf2f64167ad6f826e863297b2
Student <student@RU-102.local>
2016-01-25 16:25:41 -0700

    gold_baseComplete

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 angular.module('goldRush', [])
 angular.module('goldRush').controller('goldMapper', ['$scope', function($scope) {
-		
-		$scope.showEvent = function(event) {
-			console.log(event);
+		// $scope.showEvent = function(event) {
+		// 	console.log(event);
 
+		// }
+	
+		$scope.showIcon = false;
+		$scope.clickEvent = function(event) {
+			$scope.x = ((event.clientX) - 16);
+			$scope.y = ((event.clientY) - 37);
+			$scope.showIcon = !$scope.showIcon;
+			console.log(event.pageX);
+			console.log(event.pageY);
+		}
+		$scope.removeIcon = function () {
+			$scope.showIcon = !$scope.showIcon;
 		}
 }])
+
+
+
+
+
+
+//Testing....
+    // $scope.coords = {}
+    // $scope.y = event.clientY;
+    // &scope.x = event.clientX;
+    
+    // coords.push("X coords: " + x + ", Y coords: " + y;
