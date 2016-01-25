b24ee7bf57ea5986d6a59f91361adb11e1074ca6
Student <student@RU-102.local>
2016-01-25 11:58:57 -0700

    gold_2

---
+++ script.js
@@ -0,0 +1,8 @@
+angular.module('goldRush', [])
+angular.module('goldRush').controller('goldMapper', ['$scope', function($scope) {
+		
+		$scope.showEvent = function(event) {
+			console.log(event);
+
+		}
+}])
