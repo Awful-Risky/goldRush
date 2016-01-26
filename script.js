6cd049a772bfb190130c56150d6a455e5c9914db
Student <student@RU-102.local>
2016-01-26 16:06:09 -0700

    gold_baseComplete_bonusIComplete

--- script.js
+++ script.js
@@ -15,23 +15,27 @@ angular.module('goldRush')
 	.controller('goldMapper', ['$scope', function($scope) {
 		
 		$scope.xyNotes= []
-		// $scope.showIcon = false;
+
 		$scope.clickEvent = function(event) {
-			// $scope.x = ((event.clientX) - 16);
-			// $scope.y = ((event.clientY) - 37);
-			
-			// console.log(event.pageX);
-			// console.log(event.pageY);
-			
-			// $scope.showIcon = true;
+
 			$scope.xyNotes.push({
 				x : ((event.clientX) -16),
 				y : ((event.clientY) - 37),
+				notes : '',
+				showTextBox : false,
+				showIcon : false,
+				showToolTip: false,
 			})
 			console.log($scope.xyNotes);
 		}
 		$scope.removeIcon = function (obj) {
 			obj.showIcon = !obj.showIcon;
+		}
+
+		$scope.removeInputBox = function (obj) {
+			console.log('yes')
+			obj.showTextBox = !obj.showTextBox;
+			obj.showToolTip = !obj.showToolTip;
 		}
 }])
 
